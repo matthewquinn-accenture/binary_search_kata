a2236f0254dcfff3122b98754dd61fd5e3e1f63b
matthew quinn <matthewquinn.personal@gmail.com>
2017-06-29 15:14:03 -0400

    refactored a few lines and added in one test

--- src/main/java/BinarySearchKata/BinarySearch.java
+++ src/main/java/BinarySearchKata/BinarySearch.java
@@ -4,12 +4,24 @@ public class BinarySearch {
 
 
     public int binSearch(int numberSearch, int[] myArray) {
-        for (int i = 0; i < myArray.length; i++) {
-            if(myArray[i] == numberSearch){
-                return i;
-            }
-        }
-        return -1;
+        return searchThroughArray(0, myArray.length -1, numberSearch, myArray);
 
+    }
+
+    public int searchThroughArray(int start, int end, int numberSearch, int[] myArray){
+        int middle = ((start+end) / 2);
+
+        if(end < start)
+            return -1;
+
+        if (start == end)
+            return myArray[end] == numberSearch ? end : -1;
+
+        if (numberSearch == myArray[middle]){
+            return middle;
+        } else if (numberSearch > myArray[middle]){
+            return searchThroughArray(middle++, end, numberSearch, myArray);
+        } else
+            return searchThroughArray(start, middle--,numberSearch, myArray);
     }
 }
--- src/test/java/BinarySearchKata/BinarySearchTest.java
+++ src/test/java/BinarySearchKata/BinarySearchTest.java
@@ -14,32 +14,44 @@ public class BinarySearchTest {
     }
 
     @Test
-    public void binSearchShouldReturnIndexZeroInArrayWithOneElement(){
+    public void binSearchShouldReturnIndexZeroWhenArrayHasOneElement(){
 
         Assert.assertEquals(0, bs.binSearch(1, new int[]{1}));
 }
 
     @Test
-    public void binSearchShouldReturnIndexNegativeOneInEmptyArray(){
+    public void binSearchShouldReturnNegativeOneWhenArrayIsEmpty(){
 
         Assert.assertEquals(-1, bs.binSearch(-1, new int[]{}));
     }
 
     @Test
-    public void binSearchShouldReturnIndexNegativeOneWhenNumberSearchIsNotInArray(){
+    public void binSearchShouldReturnNegativeOneWhenNumberSearchIsNotInArray(){
 
         Assert.assertEquals(-1, bs.binSearch(2, new int[]{1}));
     }
 
     @Test
-    public void binSearchShouldReturnIndexOneWhenArrayHasThreeElement(){
+    public void binSearchShouldReturnIndexOneWhenNumberSearchIsTwoInAnArrayWithThreeElements(){
 
         Assert.assertEquals(1, bs.binSearch(2, new int[]{1,2,3}));
     }
 
     @Test
-    public void binSearchShouldReturnIndexTwoWhenArrayHasThreeElement(){
+    public void binSearchShouldReturnIndexTwoWhenNumberSearchIsThreeInAnArrayWithThreeElements(){
 
         Assert.assertEquals(2, bs.binSearch(3, new int[]{1,2,3}));
+    }
+
+    @Test
+    public void binSearchShouldReturnIndexFiveWhenNumberSearchIsSixInAnArrayWithSixElements(){
+
+        Assert.assertEquals(5, bs.binSearch(6,new int[]{1,2,3,4,5,6,7}));
+    }
+
+    @Test
+    public void binSearchShouldReturnIndexTwoWhenNumberSearchIs30InAnArrayWithThreeDoubleDigitElements(){
+
+        Assert.assertEquals(2, bs.binSearch(30, new int[]{10,20,30}));
     }
 }
